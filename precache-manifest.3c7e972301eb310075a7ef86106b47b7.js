d2084f5666111738a96d1dd3d61d050b2dfff910
Kit_EA <kit.earf@gmail.com>
2019-04-22 19:20:21 +0500

    Updates

---
+++ precache-manifest.3c7e972301eb310075a7ef86106b47b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41f14a434a108cafdb43",
+    "url": "/tvshowslist/static/js/runtime~main.9f5b317d.js"
+  },
+  {
+    "revision": "f2f3a30dcd96aaa0627c",
+    "url": "/tvshowslist/static/js/main.597b9aeb.chunk.js"
+  },
+  {
+    "revision": "53ecc8b68c4219e0019f",
+    "url": "/tvshowslist/static/js/2.ac213264.chunk.js"
+  },
+  {
+    "revision": "f2f3a30dcd96aaa0627c",
+    "url": "/tvshowslist/static/css/main.fe140e87.chunk.css"
+  },
+  {
+    "revision": "c2af1f37042f6f4bfa64ed4f4fbabc8e",
+    "url": "/tvshowslist/index.html"
+  }
+];
